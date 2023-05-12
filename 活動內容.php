c66399f18608bd65239a0d679cae02070a67ba3e
Wniii <nicochen920317@gmail.com>
2023-05-12 10:59:10 +0800

    0512

--- 活動內容.php
+++ 活動內容.php
@@ -5,7 +5,7 @@
  ?>
 <head>
     <meta charset="utf-8">
-    <title>Foody - Organic Food Website Template</title>
+    <title>輔大活動報名系統</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
